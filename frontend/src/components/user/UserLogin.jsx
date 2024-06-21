4719bf3df43d5b4a62766d5e8a3c50ea73cabea3
Sach-123 <sachinchoudhary0203@gmail.com>
2024-06-21 14:35:41 +0530

    added config in userlogin

--- frontend/src/components/user/UserLogin.jsx
+++ frontend/src/components/user/UserLogin.jsx
@@ -14,9 +14,14 @@ const Login = () => {
 
   const onSubmit = (data) => {
     axios
-      .post("https://shoe-shack-backend.vercel.app/api/v1/users/login", data)
+      .post("https://shoe-shack-backend.vercel.app/api/v1/users/login", data, {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
       .then((response) => {
-        setUsername(response.data.data.username)
+        setUsername(response.data.data.username);
         setMessage(response.data.message);
         setIsLoggedIn(true);
         setTimeout(() => {
@@ -30,41 +35,42 @@ const Login = () => {
   };
 
   return (
-      <div className="w-full flex flex-col items-center p-4 ">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col justify-center w-[300px] p-4 border-2 rounded-lg"
-        ><h1 className="my-2 text-2xl ml-1 font-bold">Login</h1>
-          <input
-            {...register("email", { required: true })}
-            className="p-2 m-1 border"
-            placeholder="Email"
-          />
-          <input
-            {...register("password", { required: true })}
-            className="p-2 m-1 border"
-            placeholder="Password"
-            type="password"
-          />
-          <input
-            type="submit"
-            className="bg-pink-700 p-2 m-1 cursor-pointer hover:drop-shadow-xl text-white"
-          />
-        </form>
-        {message === "User is not registered" ? (
-          <h1>
-            {message}.{" "}
-            <NavLink
-              to="/users/register"
-              className="underline text-red-500 hover:text-red-700"
-            >
-              Register here
-            </NavLink>
-          </h1>
-        ) : (
-          <h1>{message}</h1>
-        )}
-      </div>
+    <div className="w-full flex flex-col items-center p-4 ">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col justify-center w-[300px] p-4 border-2 rounded-lg"
+      >
+        <h1 className="my-2 text-2xl ml-1 font-bold">Login</h1>
+        <input
+          {...register("email", { required: true })}
+          className="p-2 m-1 border"
+          placeholder="Email"
+        />
+        <input
+          {...register("password", { required: true })}
+          className="p-2 m-1 border"
+          placeholder="Password"
+          type="password"
+        />
+        <input
+          type="submit"
+          className="bg-pink-700 p-2 m-1 cursor-pointer hover:drop-shadow-xl text-white"
+        />
+      </form>
+      {message === "User is not registered" ? (
+        <h1>
+          {message}.{" "}
+          <NavLink
+            to="/users/register"
+            className="underline text-red-500 hover:text-red-700"
+          >
+            Register here
+          </NavLink>
+        </h1>
+      ) : (
+        <h1>{message}</h1>
+      )}
+    </div>
   );
 };
 
